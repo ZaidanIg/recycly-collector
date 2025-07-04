fddab775fe77791ef246530f3559b349b5041391
ZaidanIg <A22100140@mhs.stmik-sumedang.ac.id>
2025-07-05 01:25:54 +0700

    add handling error but not implement in all service

--- app/services/firebase_service.py
+++ app/services/firebase_service.py
@@ -1,6 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 import traceback
+from app.errors import FirebaseError
 
 class FirebaseService:
     def __init__(self):
@@ -14,8 +15,7 @@ class FirebaseService:
             self.db = firestore.client()
             print("Firebase service initialized.")
         except Exception as e:
-            print(f"ERROR: Gagal menginisialisasi Firebase Admin SDK: {e}")
-            raise
+            raise FirebaseError("Failed to initialize Firebase Admin SDK", details=str(e))
 
     def get_session(self, session_id):
         try:
@@ -23,24 +23,21 @@ class FirebaseService:
             doc = doc_ref.get()
             return doc.to_dict() if doc.exists else None
         except Exception as e:
-            print(f"Firebase Error (get_session): {e}\n{traceback.format_exc()}")
-            return None
+            raise FirebaseError("Failed to get session from database", details=str(e))
 
     def update_session(self, session_id, data):
         try:
             self.db.collection('scan_sessions').document(session_id).set(data, merge=True)
             return True
         except Exception as e:
-            print(f"Firebase Error (update_session): {e}\n{traceback.format_exc()}")
-            return False
+            raise FirebaseError("Failed to update session", details=str(e))
 
     def set_session(self, session_id, data):
         try:
             self.db.collection('scan_sessions').document(session_id).set(data)
             return True
         except Exception as e:
-            print(f"Firebase Error (set_session): {e}\n{traceback.format_exc()}")
-            return False
+            raise FirebaseError("Failed to set session", details=str(e))
     
     def get_qr_code_data(self, session_id):
         try:
@@ -48,15 +45,13 @@ class FirebaseService:
             doc = doc_ref.get()
             return doc.to_dict() if doc.exists else None
         except Exception as e:
-            print(f"Firebase Error (get_qr_code_data): {e}\n{traceback.format_exc()}")
-            return None
+           raise FirebaseError("Failed to get qr code", details=str(e))
         
     def save_qr_code_data(self, session_id, payload):
         try:
             self.db.collection('qr_codes').document(session_id).set(payload)
             return True
         except Exception as e:
-            print(f"Firebase Error (save_qr_code_data): {e}\n{traceback.format_exc()}")
-            return False
+            raise FirebaseError("Failed to save qr code", details=str(e))
 
 firebase_service = FirebaseService()
